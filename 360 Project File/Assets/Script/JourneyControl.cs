47db47f16908aed621f6a997344b6fa32f7306c4
Ko Chi Hong <93576351+hitwuwuwu@users.noreply.github.com>
2023-09-21 09:32:07 +0800

    Finish the journey button;

--- 360 Project File/Assets/Script/JourneyControl.cs
+++ 360 Project File/Assets/Script/JourneyControl.cs
@@ -7,6 +7,12 @@ public class JourneyControl : MonoBehaviour
 
     // Update is called once per frame
     public RectTransform rectTransform;
+    [SerializeField] GameObject nextButton;
+    [SerializeField] GameObject BackButton;
+    [SerializeField] int Max;
+    int Min = 0;
+    [SerializeField]int Journey = 0;
+
     //public Vector2 newPosition;
     public bool is_up;
     public Vector2 targetPosition;
@@ -16,25 +22,45 @@ public class JourneyControl : MonoBehaviour
 
     private void Start()
     {
+        BackButton.SetActive(false);
         targetPosition.x = rectTransform.anchoredPosition.x;
         targetPosition.y = rectTransform.anchoredPosition.y;
     }
 
-    public void ChangePanelPosition()
+    public void ChangePanelPosition(bool is_up)
     {
+        this.is_up = is_up;
         isMoving = true;
-        AddPosition();
+        AddPosition(is_up);
+        checkJourney();
     }
     public void Update()
     {
         if (isMoving)
         {
-            float step = moveSpeed * Time.deltaTime;
-            rectTransform.anchoredPosition = Vector2.Lerp(rectTransform.anchoredPosition, targetPosition, step);
+            if (Journey < Max || Journey > Min)
+            {
+                float step = moveSpeed * Time.deltaTime;
+                rectTransform.anchoredPosition = Vector2.Lerp(rectTransform.anchoredPosition, targetPosition, step);
+            }
+            else if(Journey == Min || !is_up)
+            {
+                float step = moveSpeed * Time.deltaTime;
+                rectTransform.anchoredPosition = Vector2.Lerp(rectTransform.anchoredPosition, targetPosition, step);
+            }
+            else if (Journey == Max || is_up)
+            {
+                float step = moveSpeed * Time.deltaTime;
+                rectTransform.anchoredPosition = Vector2.Lerp(rectTransform.anchoredPosition, targetPosition, step);
+            }
+            if(targetPosition.y == rectTransform.anchoredPosition.y)
+            {
+                isMoving = !isMoving;
+            }
         }
 
     }
-    public void AddPosition()
+    public void AddPosition(bool is_up)
     {
         if (is_up)
         {
@@ -45,5 +71,32 @@ public class JourneyControl : MonoBehaviour
             targetPosition.y = rectTransform.anchoredPosition.y + 125;
         }
     }
+    private void checkJourney()
+    {
+        if (this.is_up)
+        {
+            Journey--;
+        }
+        else
+        {
+            Journey++;
+        }
+        if (Journey == Min)
+        {
+            BackButton.SetActive(false);
+            nextButton.SetActive(true);
+        }
+        else if(Journey == Max)
+        {
+            BackButton.SetActive(true);
+            nextButton.SetActive(false);
+        }
+        else
+        {
+            BackButton.SetActive(true);
+            nextButton.SetActive(true);
+        }
 
+
+    }
 }
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # FYP-Group-2
+using unity 2022.3.2f1
