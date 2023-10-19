8fd74c26b05f010c2283acb990a4baca0c308555
Aluminum Ken <kenchan2010@ymail.com>
2023-10-19 22:50:11 +0800

    ITCC + CX Present Build

--- 360 Project File/Assets/Scripts/Language/LanguageBehaviour.cs
+++ 360 Project File/Assets/Scripts/Language/LanguageBehaviour.cs
@@ -1,34 +1,3 @@
-using UnityEngine;
-using UnityEngine.UI;
-
-public class LanguageBehaviour : MonoBehaviour
-{
-
-    private Button myButton;
-    [SerializeField] private Language changeLanguageType;
-    [SerializeField] private GameObject targetObject;
-    private void Awake()
-    {
-        myButton = gameObject.GetComponent<Button>();
-    }
-    private void Start()
-    {
-        myButton.onClick.AddListener(() => OnButtonClick(changeLanguageType));
-    }
-
-    private void OnButtonClick(Language enumValue)
-    {
-        if(targetObject != null)
-        {
-            QuizUIManager quizUIManager = targetObject.GetComponent<QuizUIManager>();
-            if(quizUIManager != null)
-            {
-                quizUIManager.setLanguage(enumValue.ToString());
-            }
-        }
-    }
-}
-
 public enum Language
 {
     en,
