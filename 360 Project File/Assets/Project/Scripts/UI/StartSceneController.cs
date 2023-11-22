1f00b7adcfd7a5bf7b278d4b086c4dffa8134648
Aluminum Ken <kenchan2010@ymail.com>
2023-11-22 22:40:28 +0800

    StartScene Update #2
    - Also updated character glyphs for chinese fonts

---
+++ 360 Project File/Assets/Project/Scripts/UI/StartSceneController.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class StartSceneController : MonoBehaviour
+{
+    [SerializeField]
+    private CanvasGroup[] m_ManagedGroups;
+    [SerializeField]
+    private int m_InitialIndex;
+
+    [SerializeField]
+    private AnimationCurve m_SwitchAnimation = AnimationCurve.EaseInOut(0f, 0f, 0.5f, 1f);
+
+    private int m_CurrentIndex;
+
+    private void Awake()
+    {
+        foreach (var canvasGroup in m_ManagedGroups)
+        {
+            canvasGroup.alpha = 0f;
+            canvasGroup.blocksRaycasts = false;
+        }
+    }
+
+    private void Start()
+    {
+        m_ManagedGroups[m_InitialIndex].alpha = 1f;
+        m_ManagedGroups[m_InitialIndex].blocksRaycasts = true;
+        m_CurrentIndex = m_InitialIndex;
+    }
+
+    public void OpenURL(string url)
+        => Application.OpenURL(url);
+
+    public void Switch(int index)
+    {
+        Assert.IsTrue(0 <= index && index < m_ManagedGroups.Length);
+        StartCoroutine(PerformSwitch(index));
+    }
+
+    private IEnumerator PerformSwitch(int index)
+    {
+        var oldGroup = m_ManagedGroups[m_CurrentIndex];
+        var newGroup = m_ManagedGroups[index];
+
+        var time = 0f;
+        while (time < m_SwitchAnimation.GetLastKeyTime())
+        {
+            oldGroup.alpha = Mathf.Lerp(1f, 0f, m_SwitchAnimation.Evaluate(time));
+            newGroup.alpha = Mathf.Lerp(0f, 1f, m_SwitchAnimation.Evaluate(time));
+            time += Time.deltaTime;
+            yield return null;
+        }
+        oldGroup.alpha = 0f;
+        oldGroup.blocksRaycasts = false;
+        newGroup.alpha = 1f;
+        newGroup.blocksRaycasts = true;
+
+        m_CurrentIndex = index;
+    }
+}
--- 360 Project File/Assets/Project/Scripts/Utilities/ExternalLinkHelper.cs
+++ 360 Project File/Assets/Project/Scripts/Utilities/ExternalLinkHelper.cs
@@ -2,6 +2,4 @@ using UnityEngine;
 
 public class ExternalLinkHelper : MonoBehaviour
 {
-    public void OpenURL(string url)
-        => Application.OpenURL(url);
 }
