8629c29e0c120a5c186264edde78fc02bdb62ca8
Wong Yat Lam <87134224+LPMC2@users.noreply.github.com>
2024-01-30 12:54:22 +0800

    Settings Manager Initial 2

--- FYP-Terrain_Defenderion/Assets/Scripts/Manager/SettingsData.cs
+++ FYP-Terrain_Defenderion/Assets/Scripts/Manager/SettingsData.cs
@@ -7,5 +7,12 @@ public class SettingsData
 {
     [SerializeField] private float m_MusicVolume = 1f;
     [SerializeField] private float m_MainVolume = 1f;
+    [SerializeField] private ScreenType m_ScreenType;
+
     //More to place here
+    public enum ScreenType
+    {
+        FullScreen,
+        Window
+    }
 }
--- FYP-Terrain_Defenderion/Assets/Scripts/Manager/SettingsManager.cs
+++ FYP-Terrain_Defenderion/Assets/Scripts/Manager/SettingsManager.cs
@@ -6,13 +6,26 @@ using System.Collections.Generic;
 
 public class SettingsManager : MonoBehaviour
 {
+    public static SettingsManager Singleton;
     [SerializeField] private string location = "/Settings/Data";
+    [SerializeField] private string dataName = "SettingsData";
     private string settingsLocation = "";
+    [SerializeField] private  SettingsData settingsData;
+    public SettingsData SettingsData { get { return settingsData; } }
     // Start is called before the first frame update
+    private void Awake()
+    {
+        Singleton = this;
+    }
     void Start()
     {
         FolderManager.CreateFolder(location);
-        settingsLocation = Application.persistentDataPath + location;
+        settingsLocation = Application.persistentDataPath + location + dataName + ".json";
+        //Load Data
+        if(File.Exists(settingsLocation))
+        {
+            settingsData = JsonSerializer.LoadData<SettingsData>(settingsLocation);
+        }
     }
 
     // Update is called once per frame
